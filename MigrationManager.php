7ca188c85a88d238e323aeaaa613043240a1acdf
anonymframework <vahit.serif119@gmail.com>
2016-08-25 18:15:53 +0300

    made some updates

---
+++ MigrationManager.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Sagi\Database;
+
+/**
+ * Class MigrationManager
+ * @package Sagi\Database
+ */
+class MigrationManager
+{
+
+    /**
+     * @var string
+     */
+    protected $migrationTable = 'migrations';
+}
