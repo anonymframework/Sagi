3e3978904c1b7d1af1ac346f5419e74476c75260
anonymframework <vahit.serif119@gmail.com>
2016-08-25 17:39:40 +0300

    made some updates

---
+++ Console/MigrationCreateFormat.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sagi\Database\Console;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class MigrationCreateFormat
+ * @package Sagi\Database\Console
+ */
+class MigrationCreateFormat extends Command
+{
+    protected function configure()
+    {
+        $this->setName('migration:create')
+            ->setDescription('create a new migration file')
+            ->addArgument('file', InputArgument::REQUIRED, 'the name of file');
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
