a00cc311697a0e719f0c7598aef8f7f6c2d30450
anonymframework <vahit.serif119@gmail.com>
2016-08-31 19:43:53 +0300

    made some updates

---
+++ Cache.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sagi
+ * Date: 31.08.2016
+ * Time: 19:43
+ */
+
+namespace Sagi\Database;
+
+/**
+ * Class Cache
+ * @package Sagi\Database
+ */
+trait Cache
+{
+
+}
