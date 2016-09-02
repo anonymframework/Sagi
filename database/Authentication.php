73247834d871b84bd53f05baba09b4834086a542
anonymframework <vahit.serif119@gmail.com>
2016-09-02 16:40:47 +0300

    made some updates

--- database/Authentication.php
+++ database/Authentication.php
@@ -15,15 +15,21 @@ trait Authentication
      */
     public function login($datas, $remember = false)
     {
+
+        if (Identitiy::isLogined()) {
+            return Identitiy::user();
+        }
+
         if ($configs = ConfigManager::get('authentication.login')) {
 
             $username = $configs[0];
             $password = $configs[1];
 
+
             if ($this->isValidationUsed()) {
                 $this->setRules([
                     $username => 'required|digit_min:5',
-                    $password => 'required|digit_min'
+                    $password => 'required|digit_min:5'
                 ]);
 
                 $this->setFilters([
@@ -31,21 +37,19 @@ trait Authentication
                     $password => 'xss|strip_tags'
                 ]);
 
+
                 if ($this->validate($datas)) {
-
                     $datas[$password] = md5(sha1($datas[$password]));
-
-                    $find = static::find($datas);
+                    $find = static::find()
+                        ->where($username, $datas[$username])
+                        ->where($password, $datas[$password]);
 
                     if ($find->exists()) {
-
                         Identitiy::login($find, $remember);
-
                         return $find;
                     } else {
                         return false;
                     }
-
                 }
             } else {
                 throw new ModuleException('You need to use Validation module');
--- index.php
+++ index.php
@@ -2,10 +2,3 @@
 
 include "vendor/autoload.php";
 
-$user = new \Models\Users();
-
-var_dump($user->login([
-    'username' => 'vserifsaglam',
-    'password' => 'sanane123'
-]));
-
