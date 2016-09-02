a00cbfff205cf45ebc033d3b2423b6e7b3c070f1
anonymframework <vahit.serif119@gmail.com>
2016-09-02 14:51:00 +0300

    made some updates

---
+++ database/Authentication.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sagi
+ * Date: 02.09.2016
+ * Time: 14:47
+ */
+
+namespace Sagi\Database;
+
+
+trait Authentication
+{
+
+    public function login($datas)
+    {
+        if ($configs = ConfigManager::get('authentication.login')) {
+
+            $username = $configs[0];
+            $password = $configs[1];
+
+            if ($this->isValidationUsed()) {
+                $this->setRules([
+                    $username => 'required|digit_min:5',
+                    $password => 'required|digit_min'
+                ]);
+
+                $this->setFilters([
+                    $username => 'xss|strip_tags',
+                    $password => 'xss|strip_tags'
+                ]);
+
+                if ($this->validate($datas)) {
+
+                }
+            }
+
+
+        } else {
+            return false;
+        }
+    }
+
+}
