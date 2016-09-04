ff8451b2592e43eae611452803f4e9f5dc7249c1
anonymframework <vahit.serif119@gmail.com>
2016-09-04 21:08:04 +0300

    made some updates

--- database/SeedManager.php
+++ database/SeedManager.php
@@ -3,7 +3,32 @@
 namespace Sagi\Database;
 
 
-class SeedManager
+use Symfony\Component\Console\Application;
+
+abstract class SeedManager
 {
+
+    /**
+     * @var Application
+     */
+    protected $application;
+
+    /**
+     * @return Application
+     */
+    public function getApplication()
+    {
+        return $this->application;
+    }
+
+    /**
+     * @param Application $application
+     * @return SeedManager
+     */
+    public function setApplication($application)
+    {
+        $this->application = $application;
+        return $this;
+    }
 
 }
