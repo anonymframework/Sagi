21d8ca341defd10671136ebb408855d90f8d58b7
anonymframework <vahit.serif119@gmail.com>
2016-09-05 04:04:24 +0300

    made some updates

---
+++ database/Mapping/Table.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sagi\Database\Mapping;
+
+/**
+ * Class Table
+ * @package Sagi\Database\Mapping
+ */
+class Table
+{
+
+    /**
+     * the name of table
+     *
+     * @var string
+     */
+    public $name;
+}
