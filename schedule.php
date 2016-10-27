7a268aef7bf3569aac1c3710dc7df647d8bc8204
anonymframework <vahit.serif119@gmail.com>
2016-10-27 13:46:05 +0300

    made some updates

--- schedule.php
+++ schedule.php
@@ -4,3 +4,12 @@
  * @var \Sagi\Cron\Cron $schedule
  */
 
+$schedule->event(function (){
+    $task =  new \Sagi\Cron\Task\ClosureTask(function (){
+          echo 'hello world';
+    });
+
+    $task->everyMinute();
+
+    return $task;
+});
