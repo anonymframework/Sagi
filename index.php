dc50411ec8b7c73af8db1384244f4d8cdf73c1bc
anonymframework <vahit.serif119@gmail.com>
2016-11-05 19:53:51 +0300

    made some updates

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 
 include 'vendor/autoload.php';
-
-$configs = \Sagi\Database\ConfigManager::get('mvc');
-$request = new \Sagi\Http\Request();
-$app = new \Sagi\Application\App($configs, $request);
-
-$app->handleRequest();
