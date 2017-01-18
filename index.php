d00c88d43ed194f6e4dc6ebf71512560af86132d
anonymframework <vahit.serif119@gmail.com>
2017-01-19 00:39:43 +0300

    made some updates

--- index.php
+++ index.php
@@ -4,33 +4,20 @@ include 'vendor/autoload.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$old = memory_get_usage();
+$old = memory_get_usage(true);
 
-$comment = new \Models\Comments(array(
-    'text' => 'Comment Test :' . $rand = rand(),
-    'post_id' => new \Models\Posts(array(
-        'title' => 'Post test : '.$rand
-    ))
-));
-
-if(!$comment->save()){
-    throw new \Sagi\Database\QueryException($comment->error()[2]);
-}
 
 $posts = \Models\Posts::findAll();
 
-foreach ($posts as $post) {
-    echo "<br />" . $post->title ."<br />";
 
-    if ($post->comments()->exists()) {
-        foreach ($post->comments()->all() as $comment) {
-            echo "<pre>" . $comment->text . "</pre>";
-        }
-    }
-}
-echo convert(memory_get_usage()-$old);
+
+
+echo convert(memory_get_usage(true)-$old);
 
 function convert($size){
+    if ($size == 0) {
+        return ;
+    }
     $unit = array('b', 'kb', 'mb', 'gb', 'tr');
     return @round($size/pow(1024,
             ($i = floor(log($size, 1024))))).' '. $unit[$i];
