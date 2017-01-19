30ec9c5e3ecc235bbf05d933205f6be86cd68e3e
anonymframework <vahit.serif119@gmail.com>
2017-01-19 17:15:24 +0300

    made some updates

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
 include 'vendor/autoload.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$post = new \Models\Posts();
-
-$post->in('id', function (\Sagi\Database\Model $model){
-    return $model->setTable('comments')->cWhere('post_id', 'posts.id')->select('post_id');
-});
-
-$post->limit([0, 50]);
-foreach ($post as $item){
-
-    echo $item->title;
-}
-
